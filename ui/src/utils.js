7d338fff3bee55b24fbbf4fae8ed01791dcce479
johnjm2 <69920737+johnjm2@users.noreply.github.com>
2024-12-22 18:52:31 -0500

    updated1

--- ui/src/utils.js
+++ ui/src/utils.js
@@ -1,2 +1,2 @@
 export const API_URL =
-  "https://p6d7wlfcpf7ovuutyejwblgc440zpofu.lambda-url.us-east-2.on.aws/task";
+  "https://v5e6qd2paiaclyqoknqq6ff3lq0bvcsd.lambda-url.us-east-1.on.aws/task";
